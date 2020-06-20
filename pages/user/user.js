638a89fa1d8e97ee69d6961e44d146594ec8aba1
Liu Lang <hackerfollower@gmail.com>
2020-06-21 00:07:49 +0800

    add mobile and email

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,17 +1,23 @@
-const { User } = require('../../utils/av-live-query-core-min');
+const {
+  User
+} = require('../../utils/av-live-query-core-min');
 
 Page({
   data: {
     username: '',
     password: '',
+    mobilePhoneNumber: '',
+    email: '',
     error: null,
     authData: '',
   },
-  onLoad: function() {
+  onLoad: function () {
     const user = User.current();
     if (user) {
       this.setData({
         username: user.get('username'),
+        mobilePhoneNumber: user.get('mobilePhoneNumber'),
+        email: user.get('email'),
         authData: JSON.stringify(user.get('authData'), undefined, 2),
       });
     }
@@ -34,14 +40,51 @@ Page({
       password: value
     });
   },
+
+  updateMobilePhoneNumber: function ({
+    detail: {
+      value
+    }
+  }) {
+    this.setData({
+      mobilePhoneNumber: value
+    });
+  },
+  updateEmail: function ({
+    detail: {
+      value
+    }
+  }) {
+    this.setData({
+      email: value
+    });
+  },
+
+
   save: function () {
     this.setData({
       error: null,
     });
-    const { username, password } = this.data;
+    const {
+      username,
+      password,
+      mobilePhoneNumber,
+      email
+    } = this.data;
     const user = User.current();
-    if (username) user.set({ username });
-    if (password) user.set({ password });
+    if (username) user.set({
+      username
+    });
+    if (password) user.set({
+      password
+    });
+    if (mobilePhoneNumber) user.set({
+      mobilePhoneNumber
+    });
+    if (email) user.set({
+      email
+    });
+
     user.save().then(() => {
       wx.showToast({
         title: '更新成功',
